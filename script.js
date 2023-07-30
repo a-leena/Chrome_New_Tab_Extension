05b89817da9db439d8cf1526a18b3af6dd475f13
Aleena <63610006+a-leena@users.noreply.github.com>
2023-07-30 20:10:03 +0530

    uploaded all the files

---
+++ script.js
@@ -0,0 +1,28 @@
+// if (typeof window!=="undefined") {
+//     console.log("in browser");
+// }
+// else {
+//     console.log("in nodejs");
+// }
+
+function display_time() {
+    let today = new Date();
+    let date = today.toDateString();
+    let time = today.toLocaleTimeString();
+    document.getElementById("today").innerHTML = date;
+    document.getElementById('timeNow').innerHTML = time;
+    let t = setTimeout(function () { display_time() }, 1000);
+    let hour = today.getHours();
+    console.log(hour);
+    if (hour >= 4 && hour < 12) {
+        document.getElementById("greeting").innerHTML = "GOOD MORNING!";
+    }
+    else if (hour >= 12 && hour < 16) {
+        document.getElementById("greeting").innerHTML = "GOOD AFTERNOON!";
+    }
+    else {
+        document.getElementById("greeting").innerHTML = "GOOD EVENING!";
+    }
+}
+
+display_time();
